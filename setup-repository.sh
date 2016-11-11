d9ee2fb8b383229977a06841c3cd81853e6dcd61
Livre programacao ltda-me <livre.programacao@gmail.com>
2016-11-11 16:12:45 -0200

    Assunto ( subject line ) Script bash.
    Descrição ( what happened )
    Configura o repositorio git do app.

---
+++ setup-repository.sh
@@ -0,0 +1,4 @@
+
+git remote add origin git@bitbucket.org:livreprogramacao/angular-vehicles.git
+git push -u origin --all # pushes up the repo and its refs for the first time
+git push origin --tags # pushes up any tags
